91ef29c879fc29d34555e063bb43fd9859415a9d
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-03-30 20:04:01 +0300

    remote start added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,6 +15,7 @@ public class TestBase extends TestData {
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
 
