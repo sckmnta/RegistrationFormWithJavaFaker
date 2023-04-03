73e60d60ef3a75490645e02475cd778956354d38
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-04-03 20:22:55 +0300

    added system properties

--- src/test/java/tests/properties/SystemPropertiesTest.java
+++ src/test/java/tests/properties/SystemPropertiesTest.java
@@ -1,14 +1,17 @@
 package tests.properties;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Configuration.browser;
 
 public class SystemPropertiesTest {
     @Test
+    @Tag("couple_properties_test")
     void systemPropertiesTest() {
-        System.setProperty("browser", "opera");
-        String browserName = System.getProperty("browser");
+        //System.setProperty("browser", "opera");
+        String browserName = System.getProperty("browser", "mozilla");
+        String browserVersion = System.getProperty("browser_version", "97.0");
         System.out.println(browserName);
 
     }
