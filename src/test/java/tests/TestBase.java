24ed6955174cb2f2e84845150e09d5cce2fca0b5
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-04-04 22:26:34 +0300

    changed a bit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,11 +23,10 @@ public class TestBase extends TestData {
     @BeforeAll
     static void beforeall() {
         Configuration.holdBrowserOpen = true;
-        Configuration.browser = "firefox";
-        //Configuration.browserVersion = "100.0.1";
+        //Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
