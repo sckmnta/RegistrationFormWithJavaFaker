fdedd800e0f64a5e5cd01200cf4419136a6f25ef
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-04-05 15:28:50 +0300

    changed a bit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -21,11 +22,11 @@ public class TestBase extends TestData {
     @BeforeAll
     static void beforeall() {
         Configuration.holdBrowserOpen = true;
-        Configuration.browser = System.getProperty("-Dbrowser="${browser}"", "chrome");
-        Configuration.browserVersion = System.getProperty("-DbrowserVersion="${browser_version}"", "100.0");
-        Configuration.browserSize = System.getProperty("-DbrowserSize="${browser_size}"", "1920x1080");
-        Configuration.baseUrl = System.getProperty("-DbaseUrl="${base_url}"", "https://demoqa.com");
-        Configuration.remote = "https://user1:1234@" + System.getProperty("-DselenoidAdress = "${selenoid_address}"", "selenoid.autotests.cloud/wd/hub");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_address", "selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
