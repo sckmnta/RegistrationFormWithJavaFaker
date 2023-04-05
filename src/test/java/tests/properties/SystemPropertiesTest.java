e8a294944e682e5f46eac886fdcdec5e0f815bac
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-04-05 14:12:53 +0300

    changed a bit

--- src/test/java/tests/properties/SystemPropertiesTest.java
+++ src/test/java/tests/properties/SystemPropertiesTest.java
@@ -1,18 +1,18 @@
-package tests.properties;
+//package tests.properties;
 
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Tag;
+//import org.junit.jupiter.api.Test;
 
 
-public class SystemPropertiesTest {
-    @Test
-    @Tag("couple_properties_test")
-    void systemPropertiesTest() {
-        System.setProperty("browser", "opera");
-        String browserName = System.getProperty("browser", "mozilla");
-        String browserVersion = System.getProperty("browser_version", "110.0.1");
-        System.out.println(browserName);
+//public class SystemPropertiesTest {
+//    @Test
+ //   @Tag("couple_properties_test")
+ //   void systemPropertiesTest() {
+ //       System.setProperty("browser", "opera");
+ //       String browserName = System.getProperty("browser", "mozilla");
+ //       String browserVersion = System.getProperty("browser_version", "110.0.1");
+ //       System.out.println(browserName);
 
-    }
-}
+//    }
+//}
 
