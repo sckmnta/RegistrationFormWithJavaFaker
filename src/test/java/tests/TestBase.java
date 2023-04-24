50d2b425310e5fac7060108103295a27d4515ee1
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-04-24 19:55:12 +0300

    added owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,15 +18,18 @@ import java.util.Map;
 public class TestBase extends TestData {
     RegistrationPage registrationPage = new RegistrationPage();
 
+    static config.WebDriverConfig webDriverConfig = ConfigFactory.create(config.WebDriverConfig.class, System.getProperties());
+
 
     @BeforeAll
     static void beforeall() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
-        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-        Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com");
-        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
+        Configuration.baseUrl = webDriverConfig.getBaseUrl();
+        Configuration.remote = webDriverConfig.getRemoteUrl();
+        Configuration.browserSize = webDriverConfig.getBrowserSize();
+        Configuration.browser = webDriverConfig.getBrowser();
+        Configuration.browserVersion = webDriverConfig.getBrowserVersion();
+
+        Configuration.pageLoadStrategy = "eager";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
