78462f835d0cd052c4fda6066651abd47109cf1e
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-04-24 21:14:18 +0300

    -

---
+++ src/test/java/tests/properties/WebDriverConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${env}.properties"
+})
+
+public interface WebDriverConfig extends Config {
+
+    @Key("browser")
+    @DefaultValue("CHROME")
+    String getBrowser();
+
+    @Key("browser_size")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+
+    @Key("browser_version")
+    @DefaultValue("100.0")
+    String getBrowserVersion();
+
+    @Key("baseUrl")
+    @DefaultValue("https://demoqa.com")
+    String getBaseUrl();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+
+}
