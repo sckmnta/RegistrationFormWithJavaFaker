8f36f77ff39154ef87fdd77fa2945c72d39f8e56
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-05-01 22:26:16 +0300

    -

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
